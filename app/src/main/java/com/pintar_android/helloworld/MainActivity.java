1a025a0dc6361082e7421a19210ff6e4ff380a8f
Meidika <meidika.wardana.4@gmail.com>
2018-02-03 13:47:27 +0700

    menggunakan class kotlin

--- app/src/main/java/com/pintar_android/helloworld/MainActivity.java
+++ app/src/main/java/com/pintar_android/helloworld/MainActivity.java
@@ -1,13 +1,13 @@
 package com.pintar_android.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +22,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Snackbar snackbar = Snackbar.make(view, "Kotlin itu mudah", Snackbar.LENGTH_LONG);
-                snackbar.setAction("OKE", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        snackbar.dismiss();
-                    }
-                }).show();
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+
+//                final Snackbar snackbar = Snackbar.make(view, "Kotlin itu mudah", Snackbar.LENGTH_LONG);
+//                snackbar.setAction("OKE", new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        snackbar.dismiss();
+//                    }
+//                }).show();
             }
         });
     }
